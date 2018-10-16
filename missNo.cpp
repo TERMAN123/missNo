2a82ae6bc2e57fb96b3ed9a8e1acb162adf98af9
TERMAN123 <manukyan691@gmail.com>
2018-10-16 23:06:35 +0400

    Added XOR mode

--- missNo.cpp
+++ missNo.cpp
@@ -15,10 +15,30 @@ int getMissingNo(int* array, int n)
 	return sum;
 }
 
+int getMissingNoByXOR(int* arry, int n)
+{
+	int i;
+	int x1 = arry[0]; 
+	int x2 = 1;
+
+	for (i = 1; i < n; i++)
+	{
+		x1 = x1 ^ arry[i];
+	}
+	for (i = 2; i <= n + 1; i++)
+	{
+		x2 = x2 ^ i;
+	}
+
+	return (x1^x2);
+}
+
 int main()
 {
 	const int size = 5;
 	int array[size] = { 1,2,3,5,6 };
 	int missNo = getMissingNo(array, size);
-	std::cout << missNo;
+	int missNoXOR  = getMissingNoByXOR(array, size);
+	std::cout << "Missing number:" << missNo << "\n";
+	std::cout << "Missing number by XOR:" << missNoXOR;
 }
